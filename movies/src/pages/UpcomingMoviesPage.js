28a7e2f66847ffefebd447fb1ec4e6711de2ec9a
KillianGolds <20070585@mail.wit.ie>
2023-11-16 19:52:06 +0000

    New button for upcoming movies page.

--- movies/src/pages/UpcomingMoviesPage.js
+++ movies/src/pages/UpcomingMoviesPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { getUpcomingMovies } from '../api/tmdb-api';
 import PageTemplate from '../components/templateMovieListPage'
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
 
 const UpcomingMoviesPage = (props) => {
     const [movies, setMovies] = useState([]);
@@ -20,9 +21,11 @@ const UpcomingMoviesPage = (props) => {
         <PageTemplate
             title="Upcoming Movies"
             movies={movies}
-            action={(movie) => dummyAction(movie)}
+            action={(movie) => {
+                return <PlaylistAddIcon movie={movie} />
+                
+            }}
         />
     );
 }
-
 export default UpcomingMoviesPage;
--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -21,16 +21,16 @@ const HomePage = (props) => {
   // Redundant, but necessary to avoid app crashing.
   const favorites = movies.filter(m => m.favorite)
   localStorage.setItem('favorites', JSON.stringify(favorites))
-  const addToFavorites = (movieId) => true 
+  const addToFavorites = (movieId) => true
 
-  return (
-    <PageTemplate
-      title="Discover Movies"
-      movies={movies}
-      action={(movie) => {
-        return <AddToFavoritesIcon movie={movie} />
-      }}
-    />
-  );
+    return (
+      <PageTemplate
+        title="Discover Movies"
+        movies={movies}
+        action={(movie) => {
+          return <AddToFavoritesIcon movie={movie} />
+        }}
+      />
+    );
 };
 export default HomePage;
